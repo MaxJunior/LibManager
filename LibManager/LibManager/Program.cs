80e012f3ef366d2d25d929876c6cd31680478a6c
MaxJunior <maxwell989@gmail.com>
2024-11-11 04:48:46 +0000

    feat: add basic menu

---
+++ LibManager/LibManager/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+
+namespace LibManager;
+
+
+internal class Program
+{
+private static void Main(string[] args)
+{
+    WriteLine("===============| LIB MANAGER |============");
+    WriteLine("1 - Consultar Catalogo");
+    WriteLine("2 - Consultar Autores");
+    WriteLine("3 - Criar Livro ");
+    WriteLine("4 - Criar Revista ");
+    WriteLine("5 - Criar Autor");
+    WriteLine("0 - Sair");
+    WriteLine("-- Digite o comando da operação -> ");
+}
+
+}
